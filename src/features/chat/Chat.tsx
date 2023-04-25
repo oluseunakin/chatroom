c3578ae694023218605c590a77639a468da3535a
oluseunakin <akindoyinoluseun@gmail.com>
2023-04-25 21:53:18 +0100

    ggggh

--- src/features/chat/Chat.tsx
+++ src/features/chat/Chat.tsx
@@ -34,7 +34,7 @@ export const ChatComponent = () => {
   });
   return (
     <div className="chat">
-      <div>
+      <div className="close">
         <button
           onClick={() => {
             dispatch(
